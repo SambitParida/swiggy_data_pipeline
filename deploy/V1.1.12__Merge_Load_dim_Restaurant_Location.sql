5b7e66279c35fe3079f4aeecbd366a430ee83528
SambitParida <parida.sambit@gmail.com>
2025-07-07 16:00:53 -0400

    merge statement for dim res loc

--- deploy/V1.1.12__Merge_Load_dim_Restaurant_Location.sql
+++ deploy/V1.1.12__Merge_Load_dim_Restaurant_Location.sql
@@ -2,128 +2,80 @@ use role sysadmin;
 use database sandbox;
 USE WAREHOUSE compute_wh;
 
-merge into clean_sch.restaurant_location as target
+merge into CONSUMPTION_SCH.RESTAURANT_LOCATION_DIM AS target
 using
-(
-    select
-    cast(locationid as number) as location_id,
-    cast(city as string) as City,
-    case
-        when cast(state as string) = 'Delhi' then 'New Delhi'
-        else cast(state as string)
-    end as state,
-    CASE 
-            WHEN State = 'Delhi' THEN 'DL'
-            WHEN State = 'Maharashtra' THEN 'MH'
-            WHEN State = 'Uttar Pradesh' THEN 'UP'
-            WHEN State = 'Gujarat' THEN 'GJ'
-            WHEN State = 'Rajasthan' THEN 'RJ'
-            WHEN State = 'Kerala' THEN 'KL'
-            WHEN State = 'Punjab' THEN 'PB'
-            WHEN State = 'Karnataka' THEN 'KA'
-            WHEN State = 'Madhya Pradesh' THEN 'MP'
-            WHEN State = 'Odisha' THEN 'OR'
-            WHEN State = 'Chandigarh' THEN 'CH'
-            WHEN State = 'West Bengal' THEN 'WB'
-            WHEN State = 'Sikkim' THEN 'SK'
-            WHEN State = 'Andhra Pradesh' THEN 'AP'
-            WHEN State = 'Assam' THEN 'AS'
-            WHEN State = 'Jammu and Kashmir' THEN 'JK'
-            WHEN State = 'Puducherry' THEN 'PY'
-            WHEN State = 'Uttarakhand' THEN 'UK'
-            WHEN State = 'Himachal Pradesh' THEN 'HP'
-            WHEN State = 'Tamil Nadu' THEN 'TN'
-            WHEN State = 'Goa' THEN 'GA'
-            WHEN State = 'Telangana' THEN 'TG'
-            WHEN State = 'Chhattisgarh' THEN 'CG'
-            WHEN State = 'Jharkhand' THEN 'JH'
-            WHEN State = 'Bihar' THEN 'BR'
-            ELSE NULL
-        END AS state_code,
-        CASE 
-            WHEN State IN ('Delhi', 'Chandigarh', 'Puducherry', 'Jammu and Kashmir') THEN 'Y'
-            ELSE 'N'
-        END AS is_union_territory,
-        CASE 
-            WHEN (State = 'Delhi' AND City = 'New Delhi') THEN TRUE
-            WHEN (State = 'Maharashtra' AND City = 'Mumbai') THEN TRUE
-            -- Other conditions for capital cities
-            ELSE FALSE
-        END AS capital_city_flag,
-        CASE 
-            WHEN City IN ('Mumbai', 'Delhi', 'Bengaluru', 'Hyderabad', 'Chennai', 'Kolkata', 'Pune', 'Ahmedabad') THEN 'Tier-1'
-            WHEN City IN ('Jaipur', 'Lucknow', 'Kanpur', 'Nagpur', 'Indore', 'Bhopal', 'Patna', 'Vadodara', 'Coimbatore', 
-                          'Ludhiana', 'Agra', 'Nashik', 'Ranchi', 'Meerut', 'Raipur', 'Guwahati', 'Chandigarh') THEN 'Tier-2'
-            ELSE 'Tier-3'
-        END AS city_tier,
-        CAST(Zipcode as STRING) as Zip_code,
-        Cast(ActiveFlag as String) as Active_Flag,
-        TO_TIMESTAMP_TZ(CreatedDate, 'YYYY-MM-DD HH24:MI:SS') as created_ts,
-        TO_TIMESTAMP_TZ(ModifiedDate, 'YYYY-MM-DD HH24:MI:SS') as modified_ts,
-        stg_file_name,
-        stg_file_load_ts,
-        stg_file_md5,
-        current_timestamp as copy_data_ts
-        from stage_sch.location_stm
-) as source 
-on target.location_id = source.location_id
-when matched and (
-    target.City != source.City OR
-    target.State != source.State OR
-    target.state_code != source.state_code OR
-    target.is_union_territory != source.is_union_territory OR
-    target.capital_city_flag != source.capital_city_flag OR
-    target.city_tier != source.city_tier OR
-    target.Zip_Code != source.Zip_Code OR
-    target.Active_Flag != source.Active_Flag OR
-    target.modified_ts != source.modified_ts
-) THEN 
+    clean_sch.restaurant_location_stm as source 
+    on target.location_is = source.location_id and
+        target.active_flag = source.active_flag
+    
+    when matched and source.metadata$action = 'DELETE' and ource.metadata$isupdate = TRUE
+
     UPDATE SET 
-        target.City = source.City,
-        target.State = source.State,
-        target.state_code = source.state_code,
-        target.is_union_territory = source.is_union_territory,
-        target.capital_city_flag = source.capital_city_flag,
-        target.city_tier = source.city_tier,
-        target.Zip_Code = source.Zip_Code,
-        target.Active_Flag = source.Active_Flag,
-        target.modified_ts = source.modified_ts,
-        target.stg_file_name = source.stg_file_name,
-        target.stg_file_load_ts = source.stg_file_load_ts,
-        target.stg_file_md5 = source.stg_file_md5,
-        target.copy_data_ts = source.copy_data_ts
-when not matched then
+        target.eff_end_dt = current_timestamp(),
+        target.current_flag = FALSE
+
+    when matched and ource.metadata$action = 'INSERT' and ource.metadata$isupdate = TRUE
+
     insert (
-        Location_ID,
-        City,
-        State,
-        state_code,
-        is_union_territory,
-        capital_city_flag,
-        city_tier,
-        Zip_Code,
-        Active_Flag,
-        created_ts,
-        modified_ts,
-        stg_file_name,
-        stg_file_load_ts,
-        stg_file_md5,
-        copy_data_ts
+    RESTAURANT_LOCATION_HK ,
+	LOCATION_ID ,
+	CITY ,
+	STATE ,
+	STATE_CODE ,
+	IS_UNION_TERRITORY ,
+	CAPITAL_CITY_FLAG ,
+	CITY_TIER ,
+	ZIP_CODE ,
+	ACTIVE_FLAG ,
+	EFF_START_DT,
+    EFF_END_DT,
+    CURRENT_FLAG
+    ) values (
+        hash(sha1_hex(concat(source.CITY, source.STATE, source.STATE_CODE, source.ZIP_CODE))),
+        source.LOCATION_ID,
+        source.CITY,
+        source.STATE,
+        source.STATE_CODE,
+        source.IS_UNION_TERRITORY,
+        source.CAPITAL_CITY_FLAG,
+        source.CITY_TIER,
+        source.ZIP_CODE,
+        source.ACTIVE_FLAG,
+        CURRENT_TIMESTAMP(),
+        NULL,
+        TRUE
     )
-    VALUES (
-        source.Location_ID,
-        source.City,
-        source.State,
-        source.state_code,
-        source.is_union_territory,
-        source.capital_city_flag,
-        source.city_tier,
-        source.Zip_Code,
-        source.Active_Flag,
-        source.created_ts,
-        source.modified_ts,
-        source.stg_file_name,
-        source.stg_file_load_ts,
-        source.stg_file_md5,
-        source.copy_data_ts
+    when not matched
+    insert (
+    RESTAURANT_LOCATION_HK ,
+	LOCATION_ID ,
+	CITY ,
+	STATE ,
+	STATE_CODE ,
+	IS_UNION_TERRITORY ,
+	CAPITAL_CITY_FLAG ,
+	CITY_TIER ,
+	ZIP_CODE ,
+	ACTIVE_FLAG ,
+	EFF_START_DT,
+    EFF_END_DT,
+    CURRENT_FLAG
+    ) values (
+        hash(sha1_hex(concat(source.CITY, source.STATE, source.STATE_CODE, source.ZIP_CODE))),
+        source.LOCATION_ID,
+        source.CITY,
+        source.STATE,
+        source.STATE_CODE,
+        source.IS_UNION_TERRITORY,
+        source.CAPITAL_CITY_FLAG,
+        source.CITY_TIER,
+        source.ZIP_CODE,
+        source.ACTIVE_FLAG,
+        CURRENT_TIMESTAMP(),
+        NULL,
+        TRUE
     );
+
+
+
+
