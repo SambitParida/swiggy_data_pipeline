265822d1c8c469d5fbc311ead78520e59192f4a1
SambitParida <parida.sambit@gmail.com>
2025-07-10 10:47:11 -0400

    merge for clean deliveryagent

---
+++ deploy/V1.1.26__Clean_Merge_DeliveryAgent.sql
@@ -0,0 +1,80 @@
+use role sysadmin;
+
+use database sandbox;
+
+USE WAREHOUSE compute_wh;
+
+merge into
+    clean_sch.DELIVERY_AGENT as target using (
+        select
+            try_cast(DELIVERYAGENTID as number) as DELIVERY_AGENT_ID,
+            try_cast(NAME as number) as NAME,
+            try_cast(PHONE as string) as PHONE,
+            try_cast(VEHICLETYPE as string) as VEHICLE_TYPE,
+            try_cast(LOCATIONID as NUMBER) as LOCATION_ID_FK,
+            try_cast(STATUS as string) as STATUS,
+            try_cast(GENDER as string) as GENDER,
+            try_cast(RATING as NUMBER) as RATING,
+            TRY_TO_TIMESTAMP_NTZ(CreatedDate, 'YYYY-MM-DD HH24:MI:SS.FF6') as created_dt,
+            TRY_TO_TIMESTAMP_NTZ(ModifiedDate, 'YYYY-MM-DD HH24:MI:SS.FF6') as modified_dt,
+            stg_file_name,
+            stg_file_load_ts,
+            stg_file_md5,
+            current_timestamp as copy_data_ts
+        from
+            stage_sch.DELIVERYAGENT_STM
+    ) as source on target.DELIVERY_AGENT_ID = source.DELIVERY_AGENT_ID
+when matched and
+    (
+
+        target.NAME != source.NAME or 
+        target.PHONE != source.PHONE OR
+        target.VEHICLE_TYPE != source.VEHICLE_TYPE OR
+        target.LOCATION_ID_FK != source.LOCATION_ID_FK OR
+        target.STATUS != source.STATUS OR
+        target.GENDER != source.GENDER OR
+        target.RATING != source.RATING 
+
+    )
+THEN UPDATE SET
+        target.NAME = source.NAME,
+        target.PHONE = source.PHONE,
+        target.VEHICLE_TYPE = source.VEHICLE_TYPE,
+        target.LOCATION_ID_FK = source.LOCATION_ID_FK,
+        target.STATUS = source.STATUS,
+        target.GENDER = source.GENDER,
+        target.RATING = source.RATING 
+when not matched then insert
+    (
+      DELIVERY_AGENT_ID, 
+      NAME, 
+      PHONE, 
+      VEHICLE_TYPE, 
+      LOCATION_ID_FK, 
+      STATUS, 
+      GENDER, 
+      RATING,
+      CREATED_DT, 
+      MODIFIED_DT, 
+      STG_FILE_NAME, 
+      STG_FILE_LOAD_TS, 
+      STG_FILE_MD5, 
+      COPY_DATA_TS
+    )
+VALUES
+    (
+      source.DELIVERY_AGENT_ID, 
+      source.NAME, 
+      source.PHONE, 
+      source.VEHICLE_TYPE, 
+      source.LOCATION_ID_FK, 
+      source.STATUS, 
+      source.GENDER, 
+      source.RATING,
+      source.CREATED_DT, 
+      source.MODIFIED_DT, 
+      source.STG_FILE_NAME, 
+      source.STG_FILE_LOAD_TS, 
+      source.STG_FILE_MD5, 
+      source.COPY_DATA_TS
+    );
